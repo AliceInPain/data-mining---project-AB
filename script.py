1b5b639169ba3249d156324e5c787a95527a5545
AliceInPain <nazaninshafaei.ns@gmail.com>
2024-08-17 10:59:54 +0330

    total hours for each membe in a project and each team in a project

--- script.py
+++ script.py
@@ -88,22 +88,22 @@ for key, value in data.items():
             project_id = project["project_id"]
             project_team = project["team"]
             print(f"Project Name: {project_name}")
-            members_name= []
+            members_name = []
             for team_member in project_team:
                 member_name = team_member["name"]
                 tasks = team_member["tasks"]
                 print(f"Member Name: {member_name}")
-                
+
                 if tasks:
-                    tasks_name= []
+                    tasks_name = []
                     for task in tasks:
                         task_name = task["description"]
                         tasks_name.append(task_name)
                     print(f"Member Tasks: {tasks_name}")
                 else:
                     print(f"No tasks assigend to {member_name}")
-                    #--> displays:
-                    #Project Name: Project Alpha
+                    # --> displays:
+                    # Project Name: Project Alpha
                     # Member Name: Alice
                     # Member Tasks: ['Develop login module', 'Implement OAuth']
                     # Member Name: Bob
@@ -113,7 +113,26 @@ for key, value in data.items():
                     # Member Tasks: ['Design database schema', 'Develop API endpoints']
                     # Member Name: Dave
                     # No tasks assigend to Dave
+print("****************************************************************")
 
-
-
-    
+# sum of working times for each team in each project & each member in a project/team
+for project in data["projects"]:
+    project_name = project["name"]
+    total_team_hours = 0 #resetting the total team hours for each team
+    # print(project["name"])
+    for team_info in project["team"]:
+        member_name = team_info["name"]
+        # print(member_name)
+        total_member_hours = sum(task["hours_logged"]
+                                 for task in team_info["tasks"])
+        total_team_hours += total_member_hours
+        print(f"{member_name}'s total hours on {project_name}: {total_member_hours}")
+    print(f"Total Team Hours for {project_name}: {total_team_hours}")
+    #displays:
+    # Alice's total hours on Project Alpha: 15
+    # Bob's total hours on Project Alpha: 6
+    # Total Team Hours for Project Alpha: 21
+    # Charlie's total hours on Project Beta: 15
+    # Dave's total hours on Project Beta: 0
+    # Total Team Hours for Project Beta: 15
+print("****************************************************************")
---
+++ total_team_hours.py
@@ -0,0 +1,1 @@
+total_team_hours =
